4f8146fdd0bf5321c19fcaa7dd3dd5734d0e3e26
HongheeKim <gdgl0224@gmail.com>
2025-12-23 14:08:02 +0100

    Unit-Test

---
+++ src/test/java/thws/librarymanager/BookTest.java
@@ -0,0 +1,14 @@
+package thws.librarymanager;
+
+import org.junit.Test;
+import thws.librarymanager.application.domain.model.Book;
+
+import static org.junit.Assert.assertThrows;
+
+public class BookTest {
+
+    @Test
+    void shouldThrowExceptionWhenStartingLoanOnAlreadyLoanedBook() {
+
+    }
+}
