f97412eaa6f902ff9674877e64d50b9d4024e7ff
gulsum63 <166287081+gulsum63@users.noreply.github.com>
2026-01-04 14:35:59 +0100

    fix3 (#20)
    * LoanServiceTest implementieren ,out.jpa LoanStatu Enum hinzufügen...
    
    * LoanServiceTest implementieren ,out.jpa LoanStatu Enum hinzufügen...

--- src/main/java/thws/librarymanager/adapters/out/jpa/entities/LibraryEntity.java
+++ src/main/java/thws/librarymanager/adapters/out/jpa/entities/LibraryEntity.java
@@ -13,7 +13,7 @@ public class LibraryEntity {}
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)git push origin temp
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Column(nullable = false)
